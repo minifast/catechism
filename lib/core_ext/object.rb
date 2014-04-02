30de8daade4f7516ae370922444572baf54944ed
Doc Ritezel and Sarah Mei <pair+doc+sarah@ministryofvelocity.com>
2014-04-02 16:45:46 -0700

    Add inversion

--- lib/core_ext/object.rb
+++ lib/core_ext/object.rb
@@ -1,5 +1,5 @@
 class Object
-  attr_accessor :actual
+  attr_accessor :actual, :inverted
 
   def it(title, &block)
     block.call
@@ -16,8 +16,13 @@ class Object
     self
   end
 
+  def not
+    self.inverted = !self.inverted
+    self
+  end
+
   def to_equal(expected)
-    if self.actual != expected
+    if (self.actual != expected) ^ self.inverted
       raise RuntimeError.new("expected #{self.actual} to equal #{expected}")
     end
   end
